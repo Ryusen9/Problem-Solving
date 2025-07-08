992a7286727c59a48dcb926e8ca390aba923bd26
Ryusen9 <asratul02@gmail.com>
2025-07-08 22:29:52 +0600

    Add Codeforces problem: G. Katryoshka

---
+++ Codeforces/Contest 1/G_Katryoshka.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long int n, m, k;
+    cin >> n >> m >> k;
+    long long int minVal = min({n, m, k});
+    long long int newN = (n - minVal) / 2;
+    long long int newM = m - minVal;
+    long long int newK = k - minVal;
+    if (newN <= newK) {
+        cout << newN << endl;
+    } else if (newM < newK) {
+        cout << newM << endl;
+    } else {
+        cout << newK << endl;
+    }
+    return 0;
+}
