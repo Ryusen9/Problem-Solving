75e72b0179ee6f10e74878241dd435a54a7b5b7d
Ryusen9 <asratul02@gmail.com>
2025-07-12 20:58:06 +0600

    Refactor Q_Digits.cpp to improve code structure and readability

--- Codeforces/Sheet 2/Q_Digits.cpp
+++ Codeforces/Sheet 2/Q_Digits.cpp
@@ -3,6 +3,17 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        string s = to_string(num);
+        reverse(s.begin(), s.end());
+        for(int j = 0; j < s.length(); j++) {
+            cout << s[j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
